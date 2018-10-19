e28dd436b6eee43a36543d068e75da5f24d8739e
道化師 <wraith13@hotmail.co.jp>
2018-10-20 05:24:13 +0900

    🛠 phi ratio coloring ( WIP )

--- color.js
+++ color.js
@@ -17,17 +17,19 @@ var xyzToLength = function (xyz) { return Math.sqrt(Math.pow(xyz.x, 2) + Math.po
 var rgbToXyz = function (expression) { return pass_through = { x: expression.r, y: expression.g, z: expression.b }; };
 var rgbToHue = function (expression) {
     var hueXy = {
-        x: Math.sqrt(Math.pow(expression.g, 2) - Math.pow(expression.g / 2, 2))
-            - Math.sqrt(Math.pow(expression.b, 2) - Math.pow(expression.b / 2, 2)),
-        y: (expression.g / 2) + (expression.b / 2) - expression.r
+        x: expression.r - ((expression.g / 2) + (expression.b / 2)),
+        y: Math.sqrt(Math.pow(expression.g, 2) - Math.pow(expression.g / 2, 2))
+            - Math.sqrt(Math.pow(expression.b, 2) - Math.pow(expression.b / 2, 2))
     };
-    return (Math.PI / 2) - Math.atan2(hueXy.y, hueXy.x);
+    return Math.atan2(hueXy.y, hueXy.x);
 };
-var rgbToLightness = function (expression) { return xyzToLength(rgbToXyz(expression)) / xyzToLength({ x: 1.0, y: 1.0, z: 1.0 }); };
-var rgbToSaturation = function (expression) {
-    var lightness = rgbToLightness(expression);
+var rgbToLightness = function (expression) { return xyzToLength(rgbToXyz(expression)); }; // / xyzToLength({x:1.0, y:1.0, z:1.0});
+var calcSaturation = function (expression) {
+    //const lightness = xyzToLength(rgbToXyz(expression)) / xyzToLength({x:1.0, y:1.0, z:1.0});
+    var lightness = Math.sqrt(Math.pow(xyzToLength(rgbToXyz(expression)), 2) / 3);
     return xyzToLength({ x: expression.r - lightness, y: expression.g - lightness, z: expression.b - lightness });
 };
+var rgbToSaturation = function (expression) { return calcSaturation(expression); }; // /calcSaturation({r:1.0, g:0.0, b:0.0});
 var rgbToHsl = function (expression) { return pass_through =
     //	※座標空間敵に RGB 色空間の立方体の座標として捉えるので、本来であれば円筒形あるいは双円錐形の座標となる HLS (および HSV とも)厳密には異なるが、ここでは便宜上 HLS と呼称する。
     {
@@ -37,16 +39,32 @@ var rgbToHsl = function (expression) { return pass_through =
     }; };
 var hslToRgb = function (expression) {
     //	※座標空間敵に RGB 色空間の立方体の座標として捉えるので、本来であれば円筒形あるいは双円錐形の座標となる HLS (および HSV とも)厳密には異なるが、ここでは便宜上 HLS と呼称する。
-    var result = {
-        r: expression.s * Math.cos(expression.h),
-        g: expression.s * Math.cos(expression.h + (Math.PI / 3.0)),
-        b: expression.s * Math.cos(expression.h - (Math.PI / 3.0))
-    };
-    var maxLightness = xyzToLength({ x: 1.0, y: 1.0, z: 1.0 });
-    var baseLightness = rgbToLightness(result);
-    result.r += (expression.l - baseLightness) / maxLightness;
-    result.g += (expression.l - baseLightness) / maxLightness;
-    result.b += (expression.l - baseLightness) / maxLightness;
-    return result;
+    return pass_through =
+        {
+            r: Math.sqrt(Math.pow(expression.l, 2) / 3) + expression.s * Math.cos(expression.h),
+            g: Math.sqrt(Math.pow(expression.l, 2) / 3) + expression.s * Math.cos(expression.h - ((Math.PI * 2) / 3.0)),
+            b: Math.sqrt(Math.pow(expression.l, 2) / 3) + expression.s * Math.cos(expression.h + ((Math.PI * 2) / 3.0))
+        };
 };
+var test = function () {
+    console.log("rgbToHsl({r:0.0,g:0.0,b:0.0})", rgbToHsl({ r: 0.0, g: 0.0, b: 0.0 }));
+    console.log("rgbToHsl({r:1.0,g:0.0,b:0.0})", rgbToHsl({ r: 1.0, g: 0.0, b: 0.0 }));
+    console.log("rgbToHsl({r:1.0,g:1.0,b:0.0})", rgbToHsl({ r: 1.0, g: 1.0, b: 0.0 }));
+    console.log("rgbToHsl({r:1.0,g:1.0,b:1.0})", rgbToHsl({ r: 1.0, g: 1.0, b: 1.0 }));
+    console.log("rgbToHsl({r:0.5,g:0.5,b:0.5})", rgbToHsl({ r: 0.5, g: 0.5, b: 0.5 }));
+    console.log("rgbToHsl({r:0.1,g:0.0,b:0.0})", rgbToHsl({ r: 0.1, g: 0.0, b: 0.0 }));
+    console.log("rgbToHsl({r:0.1,g:0.1,b:0.0})", rgbToHsl({ r: 0.1, g: 0.1, b: 0.0 }));
+    console.log("rgbToHsl({r:0.9,g:0.0,b:0.0})", rgbToHsl({ r: 0.9, g: 0.0, b: 0.0 }));
+    console.log("rgbToHsl({r:0.9,g:0.9,b:0.0})", rgbToHsl({ r: 0.9, g: 0.9, b: 0.0 }));
+    console.log("hslToRgb(rgbToHsl({r:0.0,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({ r: 0.0, g: 0.0, b: 0.0 })), rgbToHsl({ r: 0.0, g: 0.0, b: 0.0 }).l, Math.sqrt(Math.pow(rgbToHsl({ r: 0.0, g: 0.0, b: 0.0 }).l, 2) / 3));
+    console.log("hslToRgb(rgbToHsl({r:1.0,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({ r: 1.0, g: 0.0, b: 0.0 })), rgbToHsl({ r: 1.0, g: 0.0, b: 0.0 }).l, Math.sqrt(Math.pow(rgbToHsl({ r: 1.0, g: 0.0, b: 0.0 }).l, 2) / 3));
+    console.log("hslToRgb(rgbToHsl({r:1.0,g:1.0,b:0.0}))", hslToRgb(rgbToHsl({ r: 1.0, g: 1.0, b: 0.0 })), rgbToHsl({ r: 1.0, g: 1.0, b: 0.0 }).l, Math.sqrt(Math.pow(rgbToHsl({ r: 1.0, g: 1.0, b: 0.0 }).l, 2) / 3));
+    console.log("hslToRgb(rgbToHsl({r:1.0,g:1.0,b:1.0}))", hslToRgb(rgbToHsl({ r: 1.0, g: 1.0, b: 1.0 })), rgbToHsl({ r: 1.0, g: 1.0, b: 1.0 }).l, Math.sqrt(Math.pow(rgbToHsl({ r: 1.0, g: 1.0, b: 1.0 }).l, 2) / 3));
+    console.log("hslToRgb(rgbToHsl({r:0.5,g:0.5,b:0.5}))", hslToRgb(rgbToHsl({ r: 0.5, g: 0.5, b: 0.5 })), rgbToHsl({ r: 0.5, g: 0.5, b: 0.5 }).l, Math.sqrt(Math.pow(rgbToHsl({ r: 0.5, g: 0.5, b: 0.5 }).l, 2) / 3));
+    console.log("hslToRgb(rgbToHsl({r:0.1,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({ r: 0.1, g: 0.0, b: 0.0 })), rgbToHsl({ r: 0.1, g: 0.0, b: 0.0 }).l, Math.sqrt(Math.pow(rgbToHsl({ r: 0.1, g: 0.0, b: 0.0 }).l, 2) / 3));
+    console.log("hslToRgb(rgbToHsl({r:0.1,g:0.1,b:0.0}))", hslToRgb(rgbToHsl({ r: 0.1, g: 0.1, b: 0.0 })), rgbToHsl({ r: 0.1, g: 0.1, b: 0.0 }).l, Math.sqrt(Math.pow(rgbToHsl({ r: 0.1, g: 0.1, b: 0.0 }).l, 2) / 3));
+    console.log("hslToRgb(rgbToHsl({r:0.9,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({ r: 0.9, g: 0.0, b: 0.0 })), rgbToHsl({ r: 0.9, g: 0.0, b: 0.0 }).l, Math.sqrt(Math.pow(rgbToHsl({ r: 0.9, g: 0.0, b: 0.0 }).l, 2) / 3));
+    console.log("hslToRgb(rgbToHsl({r:0.9,g:0.9,b:0.0}))", hslToRgb(rgbToHsl({ r: 0.9, g: 0.9, b: 0.0 })), rgbToHsl({ r: 0.9, g: 0.9, b: 0.0 }).l, Math.sqrt(Math.pow(rgbToHsl({ r: 0.9, g: 0.9, b: 0.0 }).l, 2) / 3));
+};
+test();
 //# sourceMappingURL=color.js.map
--- color.ts
+++ color.ts
@@ -9,9 +9,9 @@ interface ColorRgb
 }
 interface ColorHsl
 {
-    h : number; // min:0.0, max:Math.PI
-    s : number; // min:0.0, max:rgbToSaturation({r:1.0, g:0.0, b:0.0})
-    l : number; // min:0.0, max:1.0
+    h : number; // min:-Math.PI, max:Math.PI
+    s : number; // min:0.0, max:calcSaturation({r:1.0, g:0.0, b:0.0})
+    l : number; // min:0.0, max:xyzToLength({x:1.0, y:1.0, z:1.0})
 }
 interface Point3d
 {
@@ -39,17 +39,19 @@ const rgbToXyz = (expression : ColorRgb) : Point3d => pass_through = {x:expressi
 
 const rgbToHue = (expression : ColorRgb) => {
     const hueXy = {
-        x: Math.sqrt(Math.pow(expression.g, 2) -Math.pow(expression.g /2, 2))
-            -Math.sqrt(Math.pow(expression.b, 2) -Math.pow(expression.b /2, 2)),
-        y: (expression.g /2) +(expression.b /2) -expression.r
+        x: expression.r -((expression.g /2) +(expression.b /2)),
+        y: Math.sqrt(Math.pow(expression.g, 2) -Math.pow(expression.g /2, 2))
+            -Math.sqrt(Math.pow(expression.b, 2) -Math.pow(expression.b /2, 2))
     };
-    return (Math.PI/2) -Math.atan2(hueXy.y, hueXy.x);
+    return Math.atan2(hueXy.y, hueXy.x);
 };
-const rgbToLightness = (expression : ColorRgb) : number => xyzToLength(rgbToXyz(expression)) / xyzToLength({x:1.0, y:1.0, z:1.0});
-const rgbToSaturation = (expression : ColorRgb) : number => {
-    const lightness = rgbToLightness(expression);
+const rgbToLightness = (expression : ColorRgb) : number => xyzToLength(rgbToXyz(expression));// / xyzToLength({x:1.0, y:1.0, z:1.0});
+const calcSaturation = (expression : ColorRgb) : number => {
+    //const lightness = xyzToLength(rgbToXyz(expression)) / xyzToLength({x:1.0, y:1.0, z:1.0});
+    const lightness = Math.sqrt(Math.pow(xyzToLength(rgbToXyz(expression)), 2) /3);
     return xyzToLength({x:expression.r -lightness, y:expression.g -lightness, z:expression.b -lightness});
 };
+const rgbToSaturation = (expression : ColorRgb) : number => calcSaturation(expression);// /calcSaturation({r:1.0, g:0.0, b:0.0});
 const rgbToHsl = (expression : ColorRgb) : ColorHsl => pass_through =
     //	※座標空間敵に RGB 色空間の立方体の座標として捉えるので、本来であれば円筒形あるいは双円錐形の座標となる HLS (および HSV とも)厳密には異なるが、ここでは便宜上 HLS と呼称する。
     {
@@ -59,16 +61,34 @@ const rgbToHsl = (expression : ColorRgb) : ColorHsl => pass_through =
     };
 const hslToRgb = (expression : ColorHsl) : ColorRgb => {
     //	※座標空間敵に RGB 色空間の立方体の座標として捉えるので、本来であれば円筒形あるいは双円錐形の座標となる HLS (および HSV とも)厳密には異なるが、ここでは便宜上 HLS と呼称する。
-    let result =
+    return pass_through =
     {
-         r:expression.s *Math.cos(expression.h),
-         g:expression.s *Math.cos(expression.h +(Math.PI /3.0)),
-         b:expression.s *Math.cos(expression.h -(Math.PI /3.0))
+         r:Math.sqrt(Math.pow(expression.l, 2) /3) +expression.s *Math.cos(expression.h),
+         g:Math.sqrt(Math.pow(expression.l, 2) /3) +expression.s *Math.cos(expression.h -((Math.PI *2) /3.0)),
+         b:Math.sqrt(Math.pow(expression.l, 2) /3) +expression.s *Math.cos(expression.h +((Math.PI *2) /3.0))
     };
-    const maxLightness = xyzToLength({x:1.0, y:1.0, z:1.0});
-    let baseLightness = rgbToLightness(result);
-    result.r += (expression.l -baseLightness) /maxLightness;
-    result.g += (expression.l -baseLightness) /maxLightness;
-    result.b += (expression.l -baseLightness) /maxLightness;
-    return result;
 };
+
+const test = () =>
+{
+    console.log("rgbToHsl({r:0.0,g:0.0,b:0.0})", rgbToHsl({r:0.0,g:0.0,b:0.0}));
+    console.log("rgbToHsl({r:1.0,g:0.0,b:0.0})", rgbToHsl({r:1.0,g:0.0,b:0.0}));
+    console.log("rgbToHsl({r:1.0,g:1.0,b:0.0})", rgbToHsl({r:1.0,g:1.0,b:0.0}));
+    console.log("rgbToHsl({r:1.0,g:1.0,b:1.0})", rgbToHsl({r:1.0,g:1.0,b:1.0}));
+    console.log("rgbToHsl({r:0.5,g:0.5,b:0.5})", rgbToHsl({r:0.5,g:0.5,b:0.5}));
+    console.log("rgbToHsl({r:0.1,g:0.0,b:0.0})", rgbToHsl({r:0.1,g:0.0,b:0.0}));
+    console.log("rgbToHsl({r:0.1,g:0.1,b:0.0})", rgbToHsl({r:0.1,g:0.1,b:0.0}));
+    console.log("rgbToHsl({r:0.9,g:0.0,b:0.0})", rgbToHsl({r:0.9,g:0.0,b:0.0}));
+    console.log("rgbToHsl({r:0.9,g:0.9,b:0.0})", rgbToHsl({r:0.9,g:0.9,b:0.0}));
+    console.log("hslToRgb(rgbToHsl({r:0.0,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.0,g:0.0,b:0.0})), rgbToHsl({r:0.0,g:0.0,b:0.0}).l, Math.sqrt(Math.pow(rgbToHsl({r:0.0,g:0.0,b:0.0}).l, 2) /3));
+    console.log("hslToRgb(rgbToHsl({r:1.0,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:1.0,g:0.0,b:0.0})), rgbToHsl({r:1.0,g:0.0,b:0.0}).l, Math.sqrt(Math.pow(rgbToHsl({r:1.0,g:0.0,b:0.0}).l, 2) /3));
+    console.log("hslToRgb(rgbToHsl({r:1.0,g:1.0,b:0.0}))", hslToRgb(rgbToHsl({r:1.0,g:1.0,b:0.0})), rgbToHsl({r:1.0,g:1.0,b:0.0}).l, Math.sqrt(Math.pow(rgbToHsl({r:1.0,g:1.0,b:0.0}).l, 2) /3));
+    console.log("hslToRgb(rgbToHsl({r:1.0,g:1.0,b:1.0}))", hslToRgb(rgbToHsl({r:1.0,g:1.0,b:1.0})), rgbToHsl({r:1.0,g:1.0,b:1.0}).l, Math.sqrt(Math.pow(rgbToHsl({r:1.0,g:1.0,b:1.0}).l, 2) /3));
+    console.log("hslToRgb(rgbToHsl({r:0.5,g:0.5,b:0.5}))", hslToRgb(rgbToHsl({r:0.5,g:0.5,b:0.5})), rgbToHsl({r:0.5,g:0.5,b:0.5}).l, Math.sqrt(Math.pow(rgbToHsl({r:0.5,g:0.5,b:0.5}).l, 2) /3));
+    console.log("hslToRgb(rgbToHsl({r:0.1,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.1,g:0.0,b:0.0})), rgbToHsl({r:0.1,g:0.0,b:0.0}).l, Math.sqrt(Math.pow(rgbToHsl({r:0.1,g:0.0,b:0.0}).l, 2) /3));
+    console.log("hslToRgb(rgbToHsl({r:0.1,g:0.1,b:0.0}))", hslToRgb(rgbToHsl({r:0.1,g:0.1,b:0.0})), rgbToHsl({r:0.1,g:0.1,b:0.0}).l, Math.sqrt(Math.pow(rgbToHsl({r:0.1,g:0.1,b:0.0}).l, 2) /3));
+    console.log("hslToRgb(rgbToHsl({r:0.9,g:0.0,b:0.0}))", hslToRgb(rgbToHsl({r:0.9,g:0.0,b:0.0})), rgbToHsl({r:0.9,g:0.0,b:0.0}).l, Math.sqrt(Math.pow(rgbToHsl({r:0.9,g:0.0,b:0.0}).l, 2) /3));
+    console.log("hslToRgb(rgbToHsl({r:0.9,g:0.9,b:0.0}))", hslToRgb(rgbToHsl({r:0.9,g:0.9,b:0.0})), rgbToHsl({r:0.9,g:0.9,b:0.0}).l, Math.sqrt(Math.pow(rgbToHsl({r:0.9,g:0.9,b:0.0}).l, 2) /3));
+};
+
+test();
